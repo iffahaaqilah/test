64703f73b99d6d783bc6c499613fccb2c227b4f6
iffahaaqilah <21b6017@ubd.edu.bn>
2023-08-03 15:48:43 +0800

    added

---
+++ testing.py
@@ -0,0 +1,16 @@
+print("this is the first line")
+
+print("this is the another line")
+print("this is the another line")
+
+print("1234556")
+print("this is the another line")
+
+for i in range(10):
+    print("this is another line")
+
+print("this is the another line")
+print("this is the another line")
+
+print("this is the another line")
+print("this is the another line")
